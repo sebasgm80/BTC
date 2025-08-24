df37ce1f8ad3fb1d243e7c240f9c968432048b6a
sebasgm80 <156004593+sebasgm80@users.noreply.github.com>
2025-08-24 21:47:34 +0200

    Add Dashboard component with balance card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import { Calculator } from './components/Calculator/Calculator';
+import { Dashboard } from './components/Dashboard/Dashboard';
 
 function App() {
   return (
@@ -9,9 +10,11 @@ function App() {
       <Header />
       <div className='principal'>
         <Calculator />
+        <Dashboard />
       </div>
     </>
   );
 }
 
 export default App;
+
